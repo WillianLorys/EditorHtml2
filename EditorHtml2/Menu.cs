9e2db7a0d9b6bfe58a22669c443d4fc59163cacd
WillianLorys <developergrain@gmail.com>
2021-10-21 19:53:04 -0300

    Adicionar arquivos de projeto.

---
+++ EditorHtml2/Menu.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EditorHtml2
+{
+    public static class Menu
+    {
+        public static void Show()
+        {
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.Black;
+            DrawScreen();
+
+            short option = short.Parse(Console.ReadLine());
+            HandlerScreen(option);
+
+        }
+        public static void DrawScreen()
+        {
+            Console.Write("+");
+            for (int i = 0; i <= 50; i++)
+            {
+                Console.Write("-");
+            }
+            Console.Write("+");
+            Console.Write("\n");
+
+            for (int lines = 0; lines <= 20; lines++)
+            {
+                Console.Write("|");
+                for (int i = 0; i <= 50; i++)
+                {
+                    Console.Write(" ");
+                }
+                Console.Write("|");
+                Console.Write("\n");
+            }
+
+            Console.Write("+");
+            for (int i = 0; i <= 50; i++)
+            {
+                Console.Write("-");
+            } Console.Write("+");
+
+            Options();
+
+            
+        }
+        public static void Options()
+        {
+            Console.SetCursorPosition(3, 2);
+            Console.WriteLine("EDITOR HTML");
+            Console.SetCursorPosition(3, 4);
+            Console.WriteLine("============");
+            Console.SetCursorPosition(3, 6);
+            Console.WriteLine("SELECIONE UMA OPÇÃO ABAIXO:");
+            Console.SetCursorPosition(3, 8);
+            Console.WriteLine("1 - NOVO ARQUIVO");
+            Console.SetCursorPosition(3, 9);
+            Console.WriteLine("2 - ABRIR");
+            Console.SetCursorPosition(3, 10);
+            Console.WriteLine("0 - SAIR");
+            Console.SetCursorPosition(3, 12);
+            Console.Write("Opção: ");
+
+            /* selecione uma opção. novo arquivo, abrir, sair*/
+        }
+        public static void HandlerScreen(short option) 
+        {
+            switch(option)
+            {
+                case 1: Editor.ShowEdit(); break;
+                case 2: Console.WriteLine("..."); break;
+                case 0:
+                    {
+                        Console.Clear();
+                        Environment.Exit(0);
+                        break;
+                    }
+                default: Show(); break;
+            }
+        }
+    }
+}
---
+++ EditorHtml2/Viewer.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace EditorHtml2
+{
+    public static class Viewer
+    {
+        public static void Show(string text)
+        {
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Clear();
+            Console.WriteLine("MODO VISUALIZAÇÃO");
+            Console.WriteLine("-----------------");
+            Replace(text);
+            Console.WriteLine("------------------");
+            Console.ReadKey();
+        }
+        public static void Replace(string text)
+        {
+            var strong = new Regex(@"<\s*strong[^>]*>(.*?)<\s*/\s*strong>");
+            //Console.WriteLine(strong);
+
+            var worlds = text.Split(' ');
+
+            for(int i = 0; i < worlds.Length; i++)
+            {
+                if (strong.IsMatch(worlds[i]))
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.Write(worlds[i].Substring(
+                        worlds[i].IndexOf('>') + 1,
+                        (
+                            (worlds[i].LastIndexOf('<') - 1) - 
+                            worlds[i].IndexOf('>') 
+                            )
+                            )
+                        );
+                    Console.Write(" ");
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.Write(worlds[i]);
+                    Console.Write(" ");
+                }
+
+
+                
+            }
+
+            Console.WriteLine("-----------");
+            Console.WriteLine("Digite o caminho do arquivo");
+            var path = Console.ReadLine();
+            SaveRegex(path, worlds);
+
+            
+    
+        }
+
+        public static void SaveRegex(string path, string[] variable)
+        {
+            using(var file = new StreamWriter(path))
+            {
+                file.Write(variable);
+            }
+        }
+
+    }
+}
